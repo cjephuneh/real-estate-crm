44c59aef13141b4f56d0526bf83526ebc25bd7a8
caleb jephuneh <calebjephunneh@gmail.com>
2023-03-26 16:29:14 +0300

    more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Logo from '../assets/logo.png'
 import './Navbar.css'
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import FlagIcon from '@mui/icons-material/Flag';
+
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <div className='nav-logo'>
             <img src={Logo} alt=''  height={120} width={120}/>
+            
         </div>  
+        <p className='crm-desc'>Salf Real Estate CRM</p>
+        <button>
+            New lead
+        </button>
+        <div>
+            <NotificationsActiveIcon />
+        </div>
+        <div>
+            <FlagIcon />
+        </div>
+        <div>
+
+        </div>
     </nav>
       
   )
